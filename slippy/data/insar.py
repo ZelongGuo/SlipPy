6b629a478d34e59aba6f25dc63646639c7e80e9b
zelong <zelong.guo@outlook.com>
2023-12-18 22:38:22 +0100

    create dsm_quadtree

--- slippy/data/insar.py
+++ slippy/data/insar.py
@@ -13,6 +13,7 @@ __author__ = "Zelong Guo"
 __version__ = "1.0.0"
 
 import os.path
+import math
 # Standard and third-party libs
 import sys
 from typing import Optional, Union, Tuple, Dict
@@ -41,34 +42,20 @@ class InSAR(GeoTrans):
 
     """
 
-    def __init__(self,
-                 name: str,
-                 lon0: Optional[float] = None,
-                 lat0: Optional[float] = None,
-                 ellps: str = "WGS 84",
+    def __init__(self, name: str, lon0: Optional[float] = None, lat0: Optional[float] = None, ellps: str = "WGS 84",
                  utmzone: Optional[str] = None) -> None:
         # call init function of the parent class to initialize
         super().__init__(name, lon0, lat0, ellps, utmzone)
 
-        print("-------------------------------------------------------------")
+        print("x-" * 50)
         print(f"Now we initialize the InSAR instance {self.name}...")
 
         # Internal initialization
-        # format of data_ori: lon lat x y phase los azimuth incidence Ue Un Uu
         self.data = None
-        # InSAR images' parameters like width, length, corner coordinates etc.
-        self.data_para = None
-        # self.azi = None
-        # self.inc = None
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-    def read_from_gamma(self,
-                        para_file: str,
-                        phase_file: str,
-                        azi_file: str,
-                        inc_file: str,
-                        satellite: str,
+    def read_from_gamma(self, para_file: str, phase_file: str, azi_file: str, inc_file: str, satellite: str,
                         downsample: int = 3) -> None:
         """Read InSAR files (including phase, incidence, azimuth and DEM files) processed by GAMMA software
         with the parameter files, and preliminary downsampling the data if needed.
@@ -91,7 +78,7 @@ class InSAR(GeoTrans):
         # Firstly we read the para_file to get the porameters
         try:
             with open(para_file, 'r') as file:
-                print("+-" * 50)
+                print("+-" * 25 + " Original Data " + "+-" * 25)
                 print("Now we are reading the parameter file...")
                 for line in file:
                     if line.startswith('width:'):
@@ -117,7 +104,7 @@ class InSAR(GeoTrans):
             # post_arc2 = "{:.2f}".format(post_arc)
             # post_utm2 = "{:.2f}".format(post_utm)
             # print("The InSAR pixel resoluton is {} arc-second, ~{} meters." .format(post_arc2, post_utm2))
-            print(f"Range samples: {range_samples}, azimuth lines: {azimuth_lines} in {satellite} data.")
+            print(f"azimuth lines: {azimuth_lines}, Range samples: {range_samples} in {satellite} data.")
             print(f"The InSAR pixel resoluton is {post_arc:.3f} arc-second, ~{post_utm:.3f} meters.")
 
         except IOError:
@@ -132,7 +119,6 @@ class InSAR(GeoTrans):
                 incidence = np.zeros([range_samples, azimuth_lines])
 
                 # need read in column
-                print("+-" * 50)
                 print("Now we are reading the phase, azimuth and incidence images (binary files)...")
 
                 for i in range(azimuth_lines):
@@ -149,19 +135,22 @@ class InSAR(GeoTrans):
                         chunk = f3.read(4)
                         incidence[j][i] = struct.unpack('>f', chunk)[0]
 
-            print("")
-            print(f"There will be ~{int(range_samples / downsample)}x{int(azimuth_lines / downsample)} in data"
+            print(" ")
+            print("+-" * 20 + " Primarily Downsampled Data " + "+-" * 20)
+            range_samples_downsample = math.ceil(range_samples / downsample)
+            azimuth_lines_downsample = math.ceil(azimuth_lines / downsample)
+            print(f"There are {azimuth_lines_downsample}x{range_samples_downsample} in data"
                   f" with downsample factor {downsample}.")
             # make 0 values in phases, azi and inc to be Nan
             azimuth = np.where(phase == 0, np.nan, azimuth)
             incidence = np.where(phase == 0, np.nan, incidence)
             phase = np.where(phase == 0, np.nan, phase)
-            # phase and los (unit in m)
-            phase = phase.transpose().reshape(-1, 1)[::downsample]
+            # primarily downsample
+            phase = phase.transpose()[::downsample, ::downsample]
+            azimuth = azimuth.transpose()[::downsample, ::downsample]
+            incidence = incidence.transpose()[::downsample, ::downsample]
+            # los (unit in m)
             los = self.__phase2los(phase=phase, satellite=satellite)
-            # azi and inc
-            azimuth = azimuth.transpose().reshape(-1, 1)[::downsample]
-            incidence = incidence.transpose().reshape(-1, 1)[::downsample]
             # change to real azimuth and incidence with degree
             azimuth = -180 - np.degrees(azimuth)
             incidence = 90 - np.degrees(incidence)
@@ -169,13 +158,16 @@ class InSAR(GeoTrans):
             lats = np.linspace(corner_lat, corner_lat + (azimuth_lines - 1) * post_lat, azimuth_lines)
             lons = np.linspace(corner_lon, corner_lon + (range_samples - 1) * post_lon, range_samples)
             Lons, Lats = np.meshgrid(lons, lats)
-            Lons = Lons.reshape(-1, 1)[::downsample]
-            Lats = Lats.reshape(-1, 1)[::downsample]
+            Lons, Lats = Lons[::downsample, ::downsample], Lats[::downsample, ::downsample]
             # utm
             utm_x, utm_y = self.ll2xy(Lons, Lats)
 
+            post_arc_after_downsample = post_arc * downsample
+            post_utm_after_downsample = post_utm * downsample
+            print(f"The InSAR pixel resoluton is {post_arc_after_downsample:.3f} arc-second,"
+                  f" ~{post_utm_after_downsample:.3f} meters.")
+
             # now we assign attributes to the instance
-            # self.data = np.hstack([Lons, Lats, utm_x, utm_y, phase, los, azimuth, incidence])
             self.data = {
                 "lon":          {"value": Lons, "unit": "degree"},
                 "lat":          {"value": Lats, "unit": "degree"},
@@ -185,24 +177,26 @@ class InSAR(GeoTrans):
                 "los":          {"value": los, "unit": "m"},
                 "azi":          {"value": azimuth, "unit": "degree"},
                 "inc":          {"value": incidence, "unit": "degree"},
-            }
-
-            self.data_para = {
-                'satellite':                satellite,
-                'datum_name':               ellps_name
+                "parameters":   {
+                    "satellite":                        satellite,
+                    "datum_name":                       ellps_name,
+                    "downsample_factor":                downsample,
+                    "azimuth_lines_after_downsample":   azimuth_lines_downsample,
+                    "range_samples_after_downsample":   range_samples_downsample,
+                    "post_arc_after_downsample":        post_arc_after_downsample,
+                    "post_utm_after_downsample":        post_utm_after_downsample,
+                }
             }
 
         except IOError:
             print("Error: cannot open the image file, please check the file path or the parameters!")
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-    def __phase2los(self,
-                    phase: Union[float, np.ndarray],
-                    satellite: str) -> Union[float, np.ndarray]:
+    def __phase2los(self, phase: Union[float, np.ndarray], satellite: str) -> Union[float, np.ndarray]:
         """Converting InSAR phase to InSAR line-of-sight (los) disp.
 
         Args:
-            - phase:       InSAR phase data.
+            - phase:            InSAR phase data.
             - satellite:        Satellite type, "Sentinel-1", "ALOS", "ALOS-2/U" ...
 
         Returns:
@@ -230,16 +224,19 @@ class InSAR(GeoTrans):
         """Downsampling InSAR images with quadtree method.
 
         Args:
+            -
         """
+
         pass
 
+
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-    def plot(self, key_value: str, fig_name: str) -> None:
+    def plot(self, key: str, fig_name: str) -> None:
         """Plot figure to SlipPy folder in current directory.
 
         Args:
-            key_value:          Key value of data you want plotting, "los", "phase", "azi", "inc" ...
-            fig_name:           Specify a figure name without extension, .png file would be generated
+            - key:                Key value of data you want plotting, "los", "phase", "azi", "inc" ...
+            - fig_name:           Specify a figure name without extension, .png file would be generated
                                 and saved automatically.
 
         Return:
@@ -250,28 +247,27 @@ class InSAR(GeoTrans):
         folder_name = self.check_folder()
 
         # plotting
-        match key_value:
-            # plot phase
+        match key:
             case "phase" | "los" | "azi" | "inc":
-                if key_value == "phase":
-                    wrapped_phase = np.angle(np.exp(1j * self.data[key_value]["value"]))
-                    plt.scatter(self.data["lon"]["value"], self.data["lat"]["value"],
+                if key == "phase":
+                    wrapped_phase = np.angle(np.exp(1j * self.data[key]["value"].reshape(-1, 1)))
+                    plt.scatter(self.data["lon"]["value"].reshape(-1, 1), self.data["lat"]["value"].reshape(-1, 1),
                                 c=wrapped_phase, vmin=-np.pi, vmax=np.pi, cmap="rainbow")
                 else:
-                    plt.scatter(self.data["lon"]["value"], self.data["lat"]["value"],
-                                c=self.data[key_value]["value"], cmap="rainbow")
+                    plt.scatter(self.data["lon"]["value"].reshape(-1, 1), self.data["lat"]["value"].reshape(-1, 1),
+                                c=self.data[key]["value"].reshape(-1, 1), cmap="rainbow")
 
                 plt.xlabel("Longitude (deg)")
                 plt.ylabel("Latitude (deg)")
                 plt.title(f"{self.name}")
-                plt.colorbar(label=f"{key_value} [{self.data[key_value]['unit']}]")
+                plt.colorbar(label=f"{key} [{self.data[key]['unit']}]")
                 # plt.show()
                 plt.savefig(os.path.join(folder_name, fig_name + '.png'))
                 plt.close()
                 print(f"Now {fig_name} is saved to {os.path.join(folder_name, fig_name + '.png')}")
 
             case _:
-                raise ValueError(f"Key {key_value} is not in data!")
+                raise ValueError(f"Key {key} is not in data!")
 
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
